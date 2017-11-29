91749c4ef5d59eef2c61df4249ed59f4d03cce43
ZerSa <sasha-zendrikova@yandex.ru>
2017-11-29 21:44:44 +0300

    Add files via upload

--- mpi_io/mpi_io.c
+++ mpi_io/mpi_io.c
@@ -52,20 +52,21 @@ void mpi_io(void *context){
     for(i = 0; i < ctx->N; ++i){
         int x = rand() % ctx->l;
         int y = rand() % ctx->l;
-        int r = rand() % ctx->a*ctx->b;
+        int r = rand() % (ctx->a*ctx->b);
         ctx->d[(y * ctx->l + x) *ctx->a * ctx->b + r] += 1;
     }
     
     MPI_File data;
-    MPI_Datatype contiguous, view;
-    
-    MPI_File_open(MPI_COMM_WORLD, "data.bin", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &data);
-    MPI_File_set_size(data, 0);
-    
-    MPI_Type_contiguous(ctx->l * ctx->a *ctx->b, MPI_INT, &contiguous);
-    MPI_Type_create_resized(contiguous, 0, ctx->a*ctx->l*ctx->a*ctx->b*sizeof(int), &view);
-    MPI_Type_commit(&view);
-    MPI_File_set_view(data, ((comm_rank / ctx->a) * ctx->a * ctx->l + comm_rank % ctx->a) * ctx->l * ctx->a*ctx->b * sizeof(int), MPI_INT, view, "native", MPI_INFO_NULL);
+	MPI_Datatype contiguous, view;
+	
+	MPI_File_open(MPI_COMM_WORLD, "data.bin", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &data);
+	MPI_File_set_size(data, 0);
+	
+	MPI_Type_contiguous(ctx->l * ctx->a*ctx->b, MPI_INT, &contiguous);
+	MPI_Type_create_resized(contiguous, 0, ctx->a * ctx->l * ctx->a*ctx->b * sizeof(int), &view);
+	MPI_Type_commit(&view);
+	MPI_File_set_view(data, ((comm_rank / ctx->a) * ctx->a * ctx->l + comm_rank % ctx->a) * 
+			ctx->l * ctx->a*ctx->b * sizeof(int), MPI_INT, view, "native", MPI_INFO_NULL);
 	MPI_File_write_all(data, ctx->d, ctx->l * ctx->l * ctx->a*ctx->b, MPI_INT, MPI_STATUS_IGNORE);
 
 	MPI_File_close(&data);
@@ -84,21 +85,20 @@ void mpi_io(void *context){
       
 
 int main(int argc, char **argv){
+    
     MPI_Init(&argc, &argv);
-    
     if (argc == 5){
-    
-    	scalar_ctx_t ctx = {
+        scalar_ctx_t ctx = {
             .l = atoi(argv[1]),
-   	    .a = atoi(argv[2]),
-	    .b = atoi(argv[3]),
-	    .N = atoi(argv[4]),
-	    .d = 0
-	}; 
+		    .a = atoi(argv[2]),
+		    .b = atoi(argv[3]),
+		    .N = atoi(argv[4]),
+		    .d = 0
+	    }; 
 	
-	ctx.d = calloc(ctx.l * ctx.l * ctx.a * ctx.b, sizeof(int));
-	mpi_io(&ctx);
-    }
-    MPI_Finalize();
-    return 0;
+	    ctx.d = calloc(ctx.l * ctx.l * ctx.a * ctx.b, sizeof(int));
+	    mpi_io(&ctx);
+	}
+	MPI_Finalize();
+	return 0;
 }
